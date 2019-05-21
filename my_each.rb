0f947c7510b20abc8386f05f97ed976d7bda4346
ASHOK MAHARJAN <ashok997@gmail.com>
2019-05-21 17:16:07 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -10,11 +10,11 @@ def my_each(array) # put argument(s) here
  
     array
   else
-    puts "Error message"
+    print "Error message"
   end
 end
 
 collection = [1,2,3,4]
 my_each(collection) do |element|
-  print "#{element}"
+  print "#{element}"+"\n"
 end
